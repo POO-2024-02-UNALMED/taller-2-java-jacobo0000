807f10c1059ef7d6ea9bd7b87d854cca0e9fa978
jacobo0000 <jacoboleal.07@gmail.com>
2024-11-08 09:06:48 -0500

    Test 10

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -9,11 +9,11 @@ public class Auto {
   int registro;
   static String cantidadCreados;
 
-  public int cantidadAsientos(){
+  public int cantidadAsientos(x){
     int cantidad = 0
-    for (int = 0; i<this.asientos.length; i++){
-      if (this.asientos[i] instanceof Asiento){
-        cantidad +=1
+    for (int = 0; i<x.length; i++){
+      if (x[i] instanceof Asiento){
+        cantidad +=1;
       }
     }
     return cantidad;
